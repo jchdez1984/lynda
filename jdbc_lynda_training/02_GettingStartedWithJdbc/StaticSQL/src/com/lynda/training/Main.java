3920fb4879c9ec11b23446fb22b8181c52248d2e
Juan Hernandez <jiniguez@foundationmedicine.com>
2018-02-13 12:14:08 -0600

    3rd commit

---
+++ jdbc_lynda_training/02_GettingStartedWithJdbc/StaticSQL/src/com/lynda/training/Main.java
@@ -0,0 +1,34 @@
+package com.lynda.training;
+
+import java.sql.*;
+
+public class Main {
+
+    private static String USERNAME = "dbuser";
+    private static String PASSWORD = "dbpassword";
+    private static String CONN_STRING = "jdbc:mysql://localhost:3306/explorecalifornia";
+    private static final String SQL = "SELECT * FROM states";
+
+    public static void main(String[] args) throws SQLException {
+        Connection conn = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+
+        try {
+            conn = DriverManager.getConnection(CONN_STRING, USERNAME, PASSWORD);
+            stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            rs = stmt.executeQuery(SQL);
+
+            rs.last();
+            int nRows = rs.getRow();
+            System.out.println("number of retrieved rows : " + nRows);
+        } catch (SQLException e) {
+            System.err.println("The error is: " + e);
+        } finally{
+            if(conn != null){
+                conn.close();
+            }
+        }
+
+    }
+}
